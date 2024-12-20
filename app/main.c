7732e1745b1b6dd6c5c0bd01034cf611c8b54e05
Manveeranand <manveeranand88@gmail.com>
2024-12-20 23:12:20 +0530

    done with pwd.

--- app/main.c
+++ app/main.c
@@ -6,146 +6,155 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-
-enum commands{
+enum commands
+{
   EXIT,
   ECHO,
   TYPE,
-  PWD
+  PWD,
+  CD
 };
 
-
-int get_command(char* input)
+int get_command(char *input)
 {
   char *c;
-  c = strtok(input," ");
-  if(strcmp("exit",c)==0){
+  c = strtok(input, " ");
+  if (strcmp("exit", c) == 0)
+  {
     return EXIT;
   }
-  
-  if(strcmp("echo",c) == 0){
+
+  if (strcmp("echo", c) == 0)
+  {
     return ECHO;
   }
-  
-  if(strcmp("type",c) == 0){
+
+  if (strcmp("type", c) == 0)
+  {
     return TYPE;
   }
-  
-  if(strcmp("pwd",c) == 0){
+
+  if (strcmp("pwd", c) == 0)
+  {
     return PWD;
   }
   return -1;
 }
 
-bool isValid(char* path,char* args)
+bool isValid(char *path, char *args)
 {
   char filepath[100] = "";
   char *directory = strtok(path, ":");
   int size = 100;
   char fullpath[1024];
-  while(directory != NULL) 
-      {
-        snprintf(filepath,100,"%s/%s",directory,args);  
-        if (access(filepath, F_OK) == 0) {
-          return true;
-        }
-        directory = strtok(NULL, ":");
-      }
+  while (directory != NULL)
+  {
+    snprintf(filepath, 100, "%s/%s", directory, args);
+    if (access(filepath, F_OK) == 0)
+    {
+      return true;
+    }
+    directory = strtok(NULL, ":");
+  }
   return false;
 }
 
-int main() {
-  
+int main()
+{
+
   setbuf(stdout, NULL);
-  
+
   char *path = getenv("PATH");
   char abs[2024];
   strcpy(abs, path);
-  while(1){
-  strcpy(abs,path);                                                            
-  printf("$ ");
-  char *command;
-  char *token;
-  char *args;
-  char input[100];
-  fgets(input, 100, stdin);
-  input[strcspn(input, "\n")] = '\0';  //remove the trailing newline character
-  char temp[1024];
-  strcpy(temp,input);
-  command = strtok(temp," ");
-  args = strtok(NULL," ");
-  
-  int operation = get_command(input);
-  //for type command
-  char filepath[100] = "";
-  bool found = false;
-  int size = 100;
-  char fullpath[1024];
-  char cwd[1024];  
+  while (1)
+  {
+    strcpy(abs, path);
+    printf("$ ");
+    char *command;
+    char *token;
+    char *args;
+    char input[100];
+    fgets(input, 100, stdin);
+    input[strcspn(input, "\n")] = '\0'; // remove the trailing newline character
+    char temp[1024];
+    strcpy(temp, input);
+    command = strtok(temp, " ");
+    args = strtok(NULL, " ");
+
+    int operation = get_command(input);
+    // for type command
+    char filepath[100] = "";
+    bool found = false;
+    int size = 100;
+    char fullpath[1024];
+    char cwd[1024];
 
 
-//different commands logic
-  switch(operation){
-    
+    switch (operation)
+    {
+
     case EXIT:
-      if(input[5]=='0')
-      return 0;
-    
-    case ECHO:
-      printf("%.*s\n",strlen(input)-5,input+5);
-      break;
-    
-    case TYPE:
+      if (input[5] == '0')
+        return 0;
 
-      if(get_command(args)!=-1)
+    case ECHO:
+      printf("%.*s\n", strlen(input) - 5, input + 5);
+      break;
+
+    case TYPE:
+      if (get_command(args) != -1)
       {
-        printf("%s is a shell builtin\n",args);
+        printf("%s is a shell builtin\n", args);
       }
-      
-      else{
+
+      else
+      {
         char *directory = strtok(abs, ":");
-      while(directory != NULL) 
-      {
-        snprintf(filepath,100,"%s/%s",directory,args);
-        if (access(filepath, F_OK) == 0) {
-          found = true;
-          if(size>=strlen(directory))
+        while (directory != NULL)
+        {
+          snprintf(filepath, 100, "%s/%s", directory, args);
+          if (access(filepath, F_OK) == 0)
           {
-            size = strlen(directory);
-            snprintf(fullpath,1024,"%s",filepath);
+            found = true;
+            if (size >= strlen(directory))
+            {
+              size = strlen(directory);
+              snprintf(fullpath, 1024, "%s", filepath);
+            }
           }
+          directory = strtok(NULL, ":");
         }
-        directory = strtok(NULL,":");                                                                            
-      }
-      if(!found)
-        printf("%s: not found\n",args);
-      else{
+        if (!found)
+          printf("%s: not found\n", args);
+        else
+        {
           printf("%s is %s\n", args, fullpath);
-          }
+        }
       }
       break;
 
     case PWD:
-        getcwd(cwd, sizeof(cwd));
-        printf("%s\n", cwd);
-        break;
+      getcwd(cwd, sizeof(cwd));
+      printf("%s\n", cwd);
+      break;
 
-      default:
-      if(isValid(path,command))
+    default:
+      if (isValid(path, command))
       {
-      if(fork()==0){
-      execvp(command, (char *const[]){command, args, NULL});
+        if (fork() == 0)
+        {
+          execvp(command, (char *const[]){command, args, NULL});
+        }
+        else
+        {
+          wait(NULL);
+        }
       }
-      else{
-        wait(NULL);
+      else
+      {
+        printf("%s: command not found\n", command);
+      }
     }
   }
-  else{
-    printf("%s: command not found\n",command);
-  }
 }
-}
-}
-
-
-
